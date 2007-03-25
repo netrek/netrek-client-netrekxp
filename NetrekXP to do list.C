551b69550366ce79714f0959b1e743314ae10341
modemhero <modemhero>
2007-03-25 22:20:52 +0000

    Changed border refresh to happen at 10/second for all bitmap sets rather than just the high res bitmaps.  As now there are several things that ignore borders (high res ships, range circles) it just makes more sense to redraw borders semi-constantly.

--- NetrekXP to do list.C
+++ NetrekXP to do list.C
@@ -47,7 +47,6 @@ rect doesn't take these into account.
 to hostname not resolving.  Currently fixed by using mulitple attempts to resolve bad
 host names.
 11) mouse click repeat rate
-12) range circle overwrite border with anything but HR bitmap set
 13) _ bug with backspace and messages in HUD
 
 Stas's list:
--- src/local.c
+++ src/local.c
@@ -2472,11 +2472,11 @@ DrawMisc (void)
             break;
         }
     }
-    /* Force a border redraw? Only needed for bitmaps rotated realtime, due 
-       to the overwriting border issue.  Also, very CPU expensive to write
+    /* Force a border redraw?  Bitmaps rotated realtime as well as viewRange circles 
+       will overwrite the border.  Since it is very CPU expensive to write
        rectangles (drawborder function) to the active window, especially if double
-       buffering is off, so let's slow down redraws to at most 10 per second */
-    else if (colorClient == 4)
+       buffering is off, let's slow down redraws to at most 10 per second */
+    else
     {
     	static int border_refresh = 0;
 
